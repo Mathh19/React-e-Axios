9d835903f6f838667ae8c0a6ab00382f52aaf8d7
Matheus <mf635097@gmail.com>
2022-12-06 19:32:05 -0300

    Stylization made from route to create post

--- src/routes/NewPost/index.tsx
+++ src/routes/NewPost/index.tsx
@@ -1,5 +1,18 @@
+import './styles.css';
+
 export const NewPost = () => {
   return (
-    <div>NewPost</div>
+    <div className="new-post">
+      <h2>Inserir novo Post:</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input type="text" name="title" id="title" placeholder="Digite o título" />
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea name="body" id="body" placeholder="Digite o título"></textarea>
+        </div>
+        <input type="submit" value="Criar post" className='btn'/>
+      </form>
+    </div>
   )
 }
